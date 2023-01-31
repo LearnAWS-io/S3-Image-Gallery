8285c64c9a2f158c77791cb01ce991b265a29e06
Shivam <singhshivamkr@gmail.com>
2023-02-01 01:54:03 +0530

    :lock: Add https cert config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from "astro/config";
-
+import fs from "fs";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  vite: {
+    server: {
+      https: {
+        // TODO: generate your own certificate using: mkcert domain.com
+        key: fs.readFileSync("./local.nootnoot.in-key.pem"),
+        cert: fs.readFileSync("./local.nootnoot.in.pem"),
+      },
+    },
+  },
 });
