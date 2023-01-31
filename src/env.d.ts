a931207d57841110a0a51be91a6629ca9e0b35b6
Shivam <singhshivamkr@gmail.com>
2023-02-01 01:55:21 +0530

    :beers: Add env definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,15 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
   readonly S3_BUCKET_NAME: string;
-  readonly PUBLIC_POKEAPI: string;
+
+  //NOTE: private key needs to be base64 encoded without newlines
+  readonly CF_PRIVATE_KEY: string;
+  readonly CF_KEYPAIR_ID: string;
+  readonly CF_DOMAIN: string;
+
+  //NOTE: IAM user must have access to S3
+  readonly AWS_ACCESS_KEY_ID: string;
+  readonly AWS_ACCESS_KEY_SECRET: string;
   // more env variables...
 }
 
